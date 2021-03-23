f48284d6e1e0d813c57aa2fe6339369c1c5297bd
HuoHaodong <inf1nityhhd@163.com>
2021-03-23 16:40:08 +0800

    实验5-1新版本

--- LAB5/LAB5_1.py
+++ LAB5/LAB5_1.py
@@ -8,6 +8,7 @@ import pandas as pd
 import utils
 from matplotlib import pyplot as plt
 import time
+import numpy as np
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -20,91 +21,52 @@ URI6 = 'radio://0/6/2M'
 URI7 = 'radio://0/7/2M'
 URI8 = 'radio://0/8/2M'
 URI9 = 'radio://0/9/2M'
+URI10 = 'radio://0/10/2M'
+URI11 = 'radio://0/11/2M'
+URIS = [URI1, URI2, URI3, URI4, URI5, URI6, URI7, URI8, URI9, URI10, URI11]
 
 
-def plot():
-    # 开启上下车前
-    data1_1 = pd.read_csv('../data/LAB5_1_1_A.csv').apply(pd.to_numeric)
-    data2_1 = pd.read_csv('../data/LAB5_1_1_B.csv').apply(pd.to_numeric)
-    data3_1 = pd.read_csv('../data/LAB5_1_1_C.csv').apply(pd.to_numeric)
-    data4_1 = pd.read_csv('../data/LAB5_1_1_D.csv').apply(pd.to_numeric)
-    data5_1 = pd.read_csv('../data/LAB5_1_1_E.csv').apply(pd.to_numeric)
-
-    data1_1 = (data1_1 - data1_1.iloc[0]).iloc[500]
-    data2_1 = (data2_1 - data2_1.iloc[0]).iloc[500]
-    data3_1 = (data3_1 - data3_1.iloc[0]).iloc[500]
-    data4_1 = (data4_1 - data4_1.iloc[0]).iloc[500]
-    data5_1 = (data5_1 - data5_1.iloc[0]).iloc[500]
-
-    # 开启上下车后
-    data1_2 = pd.read_csv('../data/LAB5_1_2_A.csv').apply(pd.to_numeric)
-    data2_2 = pd.read_csv('../data/LAB5_1_2_B.csv').apply(pd.to_numeric)
-    data3_2 = pd.read_csv('../data/LAB5_1_2_C.csv').apply(pd.to_numeric)
-    data4_2 = pd.read_csv('../data/LAB5_1_2_D.csv').apply(pd.to_numeric)
-    data5_2 = pd.read_csv('../data/LAB5_1_2_E.csv').apply(pd.to_numeric)
-
-    data1_2 = (data1_2 - data1_2.iloc[0]).iloc[500]
-    data2_2 = (data2_2 - data2_2.iloc[0]).iloc[500]
-    data3_2 = (data3_2 - data3_2.iloc[0]).iloc[500]
-    data4_2 = (data4_2 - data4_2.iloc[0]).iloc[500]
-    data5_2 = (data5_2 - data5_2.iloc[0]).iloc[500]
-
-    # 整合数据
-    labels = ['A', 'B', 'C', 'D', 'E']
-    data = pd.DataFrame(dtype='float64', columns=labels)
-    data = data.append({
-        'A': data1_1['total_compute'] / (len(labels) - 1), 'B': data2_1['total_compute'] / (len(labels) - 1),
-        'C': data3_1['total_compute'] / (len(labels) - 1), 'D': data4_1['total_compute'] / (len(labels) - 1),
-        'E': data5_1['total_compute'] / (len(labels) - 1)
-    }, ignore_index=True)
-    data = data.append({
-        'A': data1_2['total_compute'] / (len(labels) - 1), 'B': data2_2['total_compute'] / (len(labels) - 1),
-        'C': data3_2['total_compute'] / (len(labels) - 1), 'D': data4_2['total_compute'] / (len(labels) - 1),
-        'E': data5_2['total_compute'] / (len(labels) - 1)
-    }, ignore_index=True)
+def plot(before, after, labels=['A', 'B', 'C', 'D', 'E']):
+    data = np.asarray([before, after])
+    data = pd.DataFrame(data=data, dtype='float64', columns=labels)
+    print(data)
+    data = data / (len(labels) - 1)
     # 绘图
     data.T.plot(kind='bar')
     plt.xticks(fontproperties='Times New Roman', rotation=0)
     plt.ylabel('Average Ranging Count')
     plt.legend(['before', 'after'], framealpha=0)
-    plt.savefig('../imgs/LAB5-1.jpg')
+    # plt.savefig('../imgs/LAB5-1.jpg')
     plt.show()
 
 
 if __name__ == '__main__':
     log_var = {
-        'receive_from_2': 'uint16_t',
-        'receive_from_3': 'uint16_t',
-        'receive_from_4': 'uint16_t',
-        'receive_from_5': 'uint16_t',
-        'compute_from_2': 'uint16_t',
-        'compute_from_3': 'uint16_t',
-        'compute_from_4': 'uint16_t',
-        'compute_from_5': 'uint16_t',
         'total_receive': 'uint16_t',
         'total_send': 'uint16_t',
         'total_compute': 'uint16_t'
     }
-    # LAB5_1_1_A 实验5的第一个实验的第一种情况（不开启上下车机制），无人机A的情况
-    # LAB5_1_2_X 实验5的第一个实验的第二种情况（开启上下车机制），无人机X的情况
-    utils.log_ranging(link_uri=URI1, log_cfg_name='TSranging', log_save_path='../data/LAB5_1_1_A.csv', log_var=log_var,
-                      period_in_ms=100, keep_time_in_s=70)
-    utils.log_ranging(link_uri=URI2, log_cfg_name='TSranging', log_save_path='../data/LAB5_1_1_B.csv', log_var=log_var,
-                      period_in_ms=100, keep_time_in_s=70)
-    utils.log_ranging(link_uri=URI3, log_cfg_name='TSranging', log_save_path='../data/LAB5_1_1_C.csv', log_var=log_var,
-                      period_in_ms=100, keep_time_in_s=70)
-    utils.log_ranging(link_uri=URI4, log_cfg_name='TSranging', log_save_path='../data/LAB5_1_1_D.csv', log_var=log_var,
-                      period_in_ms=100, keep_time_in_s=70)
-    utils.log_ranging(link_uri=URI5, log_cfg_name='TSranging', log_save_path='../data/LAB5_1_1_E.csv', log_var=log_var,
-                      period_in_ms=100, keep_time_in_s=70)
-    utils.log_ranging(link_uri=URI1, log_cfg_name='TSranging', log_save_path='../data/LAB5_1_2_A.csv', log_var=log_var,
-                      period_in_ms=100, keep_time_in_s=70)
-    utils.log_ranging(link_uri=URI2, log_cfg_name='TSranging', log_save_path='../data/LAB5_1_2_B.csv', log_var=log_var,
-                      period_in_ms=100, keep_time_in_s=70)
-    utils.log_ranging(link_uri=URI3, log_cfg_name='TSranging', log_save_path='../data/LAB5_1_2_C.csv', log_var=log_var,
-                      period_in_ms=100, keep_time_in_s=70)
-    utils.log_ranging(link_uri=URI4, log_cfg_name='TSranging', log_save_path='../data/LAB5_1_2_D.csv', log_var=log_var,
-                      period_in_ms=100, keep_time_in_s=70)
-    utils.log_ranging(link_uri=URI5, log_cfg_name='TSranging', log_save_path='../data/LAB5_1_2_E.csv', log_var=log_var,
-                      period_in_ms=100, keep_time_in_s=70)
-    plot()
+    data1 = []
+    data2 = []
+    for URI in URIS:
+        temp = utils.log_ranging(link_uri=URI,
+                                 log_cfg_name='TSranging',
+                                 log_var=log_var,
+                                 period_in_ms=100,
+                                 keep_time_in_s=2)
+        data1.append(temp['total_compute'][temp.shape[0] - 1])
+
+    time.sleep(180)
+    for URI in URIS:
+        temp = utils.log_ranging(link_uri=URI,
+                                 log_cfg_name='TSranging',
+                                 log_var=log_var,
+                                 period_in_ms=100,
+                                 keep_time_in_s=2)
+        data2.append(temp['total_compute'][0])
+    labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
+    result = np.asarray([data1, data2])
+    result = (result - result[0])[-1]
+    print(result)
+
+    # plot(before=data1, after=data2, labels=labels)
